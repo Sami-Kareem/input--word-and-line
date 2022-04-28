6053ee75814f60d7d21def58eb1e282ccfe20111
sami kareem <103708057+Sami-Kareem@users.noreply.github.com>
2022-04-28 14:03:40 +0500

    Add files via upload

---
+++ ClassTask.java
@@ -0,0 +1,19 @@
+	import java.util.Scanner ;
+class ClassTask{
+
+	public static void main (String sami[]){
+	  Scanner obj=new Scanner(System.in);
+		System.out.println("Enter a line ");
+	   String line= obj.nextLine();
+		System.out.println(line);
+
+
+		System.out.println("Enter a word ");
+	   String word= obj.next();
+		System.out.println(word);
+
+
+
+  }
+
+}
